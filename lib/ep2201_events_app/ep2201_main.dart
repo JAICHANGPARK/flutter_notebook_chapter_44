8d192b508ce500102ad3d2f04ed31ed715b870fd
JAICHANGPARK <aristojeff@gmail.com>
2025-03-30 09:26:59 +0900

    Update ep2201_main.dart

--- lib/ep2201_events_app/ep2201_main.dart
+++ lib/ep2201_events_app/ep2201_main.dart
@@ -4,3 +4,12 @@ import 'package:flutter/material.dart';
 void main() {
   
 }
+
+class EventsMobileApp extends StatelessWidget {
+  const EventsMobileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
