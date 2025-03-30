267a2f98c0696c621dd3d2624129d2ab40f38d89
JAICHANGPARK <aristojeff@gmail.com>
2025-03-30 09:30:55 +0900

    Create events_app_home_page.dart

---
+++ lib/ep2201_events_app/ui/events_app_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EventsAppHomePage extends StatefulWidget {
+  const EventsAppHomePage({super.key});
+
+  @override
+  State<EventsAppHomePage> createState() => _EventsAppHomePageState();
+}
+
+class _EventsAppHomePageState extends State<EventsAppHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
