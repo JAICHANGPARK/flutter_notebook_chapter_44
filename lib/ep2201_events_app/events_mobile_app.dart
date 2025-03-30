e145ec0c667c68c68ca5603452fca1bab7a0a391
JAICHANGPARK <aristojeff@gmail.com>
2025-03-30 09:27:27 +0900

    Update events_mobile_app.dart

---
+++ lib/ep2201_events_app/events_mobile_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EventsMobileApp extends StatelessWidget {
+  const EventsMobileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
