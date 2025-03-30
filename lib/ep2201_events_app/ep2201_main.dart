24de9d851d2b42b5719fce2a4c4ca087020df5f9
JAICHANGPARK <aristojeff@gmail.com>
2025-03-30 09:27:38 +0900

    Update ep2201_main.dart

--- lib/ep2201_events_app/ep2201_main.dart
+++ lib/ep2201_events_app/ep2201_main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_44/ep2201_events_app/events_mobile_app.dart';
 
 
 void main() {
-  
+  runApp(EventsMobileApp());
 }
 
