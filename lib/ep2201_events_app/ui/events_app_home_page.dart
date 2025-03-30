1468c312240fa905db5087e024a49ffd49f19d47
JAICHANGPARK <aristojeff@gmail.com>
2025-03-30 09:36:12 +0900

    Update events_app_home_page.dart

--- lib/ep2201_events_app/ui/events_app_home_page.dart
+++ lib/ep2201_events_app/ui/events_app_home_page.dart
@@ -17,13 +17,16 @@ class _EventsAppHomePageState extends State<EventsAppHomePage> {
         crossAxisAlignment: CrossAxisAlignment.start,
         // spacing: 12,
         children: [
-          Expanded(child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(24),
-              )
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(24),
+                ),
+                color: Color.fromRGBO(245, 244, 249, 1),
+              ),
             ),
-          )),
+          ),
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: Row(
@@ -33,36 +36,28 @@ class _EventsAppHomePageState extends State<EventsAppHomePage> {
                   spacing: 6,
                   children: [
                     Icon(HugeIcons.strokeRoundedHome01),
-                    Text("Home", style: TextStyle(
-                      fontSize: 12,
-                    )),
+                    Text("Home", style: TextStyle(fontSize: 12)),
                   ],
                 ),
                 Column(
                   spacing: 6,
                   children: [
                     Icon(HugeIcons.strokeRoundedDiscoverCircle),
-                    Text("History", style: TextStyle(
-                      fontSize: 12,
-                    )),
+                    Text("History", style: TextStyle(fontSize: 12)),
                   ],
                 ),
                 Column(
                   spacing: 6,
                   children: [
                     Icon(HugeIcons.strokeRoundedCalendar01),
-                    Text("Bookings", style: TextStyle(
-                      fontSize: 12,
-                    )),
+                    Text("Bookings", style: TextStyle(fontSize: 12)),
                   ],
                 ),
                 Column(
                   spacing: 6,
                   children: [
                     Icon(HugeIcons.strokeRoundedUser),
-                    Text("Profile", style: TextStyle(
-                      fontSize: 12,
-                    )),
+                    Text("Profile", style: TextStyle(fontSize: 12)),
                   ],
                 ),
               ],
