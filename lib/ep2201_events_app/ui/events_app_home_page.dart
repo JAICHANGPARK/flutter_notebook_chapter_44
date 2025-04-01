02b1edeaee150f7088641aaecce30fb5ae607804
JAICHANGPARK <aristojeff@gmail.com>
2025-04-01 19:34:54 +0900

    Update events_app_home_page.dart

--- lib/ep2201_events_app/ui/events_app_home_page.dart
+++ lib/ep2201_events_app/ui/events_app_home_page.dart
@@ -102,6 +102,24 @@ class _EventsAppHomePageState extends State<EventsAppHomePage> {
                                       ),
                                     ),
                                   ),
+                                  Positioned.fill(
+                                    left: 24,
+                                    right: 24,
+                                    bottom: 24,
+                                    top: 24,
+                                    child: Transform.rotate(
+                                      angle: 0,
+                                      child: Container(
+                                        margin: EdgeInsets.all(12),
+                                        decoration: BoxDecoration(
+                                          color: Colors.lightGreenAccent,
+                                          borderRadius: BorderRadius.circular(
+                                            16,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
                                 ],
                               ),
                             ),
