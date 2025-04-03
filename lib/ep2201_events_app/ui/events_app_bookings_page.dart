454bd8492ec23fc11824a59f7b8d4f5e9d392c5a
JAICHANGPARK <aristojeff@gmail.com>
2025-04-03 19:22:27 +0900

    Update events_app_bookings_page.dart

---
+++ lib/ep2201_events_app/ui/events_app_bookings_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class EventsAppBookingsPage extends StatefulWidget {
+  const EventsAppBookingsPage({super.key});
+
+  @override
+  State<EventsAppBookingsPage> createState() => _EventsAppBookingsPageState();
+}
+
+class _EventsAppBookingsPageState extends State<EventsAppBookingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
