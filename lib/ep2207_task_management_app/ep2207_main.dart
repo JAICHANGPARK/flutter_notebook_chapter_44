9e2bf55627c5124c4f0708cc65b86a36ac2d3fcb
JAICHANGPARK <aristojeff@gmail.com>
2025-04-07 18:37:19 +0900

    Create ep2207_main.dart

---
+++ lib/ep2207_task_management_app/ep2207_main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'ui/task_management_main_page.dart';
+
+void main() {
+  runApp(TaskManagementApp());
+}
+
+class TaskManagementApp extends StatelessWidget {
+  const TaskManagementApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TaskManagementMainPage(),
+    );
+  }
+}
