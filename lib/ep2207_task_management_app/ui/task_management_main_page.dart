b542e814f1a5e8bc8990d8655f171978acdf1c64
JAICHANGPARK <aristojeff@gmail.com>
2025-04-07 18:48:44 +0900

    Update task_management_main_page.dart

--- lib/ep2207_task_management_app/ui/task_management_main_page.dart
+++ lib/ep2207_task_management_app/ui/task_management_main_page.dart
@@ -146,93 +146,99 @@ class _TaskManagementMainPageState extends State<TaskManagementMainPage> {
                           color: Colors.white,
                           borderRadius: BorderRadius.circular(12),
                         ),
-                        padding: EdgeInsets.all(12),
+                        // padding: EdgeInsets.all(12),
                         child: Column(
                           spacing: 10,
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text(
-                              "Finance Mobile App",
-                              style: TextStyle(fontSize: 24),
-                            ),
-                            Text(
-                              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-                              maxLines: 3,
-                              style: TextStyle(
-                                fontSize: 12,
-                                color: Colors.black54,
-                              ),
-                            ),
-                            Row(
-                              spacing: 8,
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Container(
-                                  decoration: ShapeDecoration(
-                                    shape: StadiumBorder(),
-                                    color: Colors.grey[100]!,
-                                  ),
-                                  child: Text("In Progress"),
-                                  padding: EdgeInsets.symmetric(
-                                    horizontal: 16,
-                                    vertical: 6,
+                                Text(
+                                  "Finance Mobile App",
+                                  style: TextStyle(fontSize: 24),
+                                ),
+                                Text(
+                                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+                                  maxLines: 3,
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.black54,
                                   ),
                                 ),
+                                Row(
+                                  spacing: 8,
+                                  children: [
+                                    Container(
+                                      decoration: ShapeDecoration(
+                                        shape: StadiumBorder(),
+                                        color: Colors.grey[100]!,
+                                      ),
+                                      child: Text("In Progress"),
+                                      padding: EdgeInsets.symmetric(
+                                        horizontal: 16,
+                                        vertical: 6,
+                                      ),
+                                    ),
 
-                                Container(
-                                  decoration: ShapeDecoration(
-                                    shape: StadiumBorder(),
-                                    color: Colors.grey[100]!,
-                                  ),
-                                  child: Text("Medium"),
-                                  padding: EdgeInsets.symmetric(
-                                    horizontal: 16,
-                                    vertical: 6,
-                                  ),
+                                    Container(
+                                      decoration: ShapeDecoration(
+                                        shape: StadiumBorder(),
+                                        color: Colors.grey[100]!,
+                                      ),
+                                      child: Text("Medium"),
+                                      padding: EdgeInsets.symmetric(
+                                        horizontal: 16,
+                                        vertical: 6,
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                              ],
-                            ),
-                            Container(
-                              height: 52,
-                              margin: EdgeInsets.symmetric(vertical: 8),
-                              child: Row(
-                                spacing: 12,
-                                children: [
-                                  Container(
-                                    width: 120,
-                                    child: Stack(
-                                      children:
+                                Container(
+                                  height: 52,
+                                  margin: EdgeInsets.symmetric(vertical: 8),
+                                  child: Row(
+                                    spacing: 12,
+                                    children: [
+                                      Container(
+                                        width: 120,
+                                        child: Stack(
+                                          children:
                                           List.generate(3, (idx) {
                                             return Positioned(
                                               left: idx * 34,
                                               child: CircleAvatar(radius: 26),
                                             );
                                           }).toList(),
-                                    ),
-                                  ),
-                                  Expanded(
-                                    child: Text.rich(
-                                      TextSpan(
-                                        children: [
-                                          TextSpan(
-                                            text: "3 People ",
-                                            style: TextStyle(
-                                              color: Colors.black,
-                                              fontWeight: FontWeight.bold,
-                                            ),
-                                          ),
-                                          TextSpan(
-                                            text: "in this project",
-                                            style: TextStyle(
-                                              color: Colors.black87,
-                                            ),
-                                          ),
-                                        ],
+                                        ),
                                       ),
-                                    ),
+                                      Expanded(
+                                        child: Text.rich(
+                                          TextSpan(
+                                            children: [
+                                              TextSpan(
+                                                text: "3 People ",
+                                                style: TextStyle(
+                                                  color: Colors.black,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                              TextSpan(
+                                                text: "in this project",
+                                                style: TextStyle(
+                                                  color: Colors.black87,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                ],
-                              ),
+                                ),
+                              ],
                             ),
+
                             Container(
                               // height: 52,
                               padding: EdgeInsets.all(8),
