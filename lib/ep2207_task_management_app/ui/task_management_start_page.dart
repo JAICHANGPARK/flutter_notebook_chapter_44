25366234e51df3a64e2e2eb6452a1f8e06af292e
JAICHANGPARK <aristojeff@gmail.com>
2025-04-08 18:44:58 +0900

    Update task_management_start_page.dart

--- lib/ep2207_task_management_app/ui/task_management_start_page.dart
+++ lib/ep2207_task_management_app/ui/task_management_start_page.dart
@@ -39,6 +39,33 @@ class _TaskManagementStartPageState extends State<TaskManagementStartPage> {
             ),
           ),
           Positioned.fill(
+            top: 240,
+            left: 72,
+            right: -42,
+
+            child: Transform(
+              transform:
+              Matrix4.identity()
+                ..rotateX(0.2)
+                ..rotateY(.4)
+                ..rotateZ(-.1),
+              child: Container(
+                padding: EdgeInsets.all(3),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withValues(alpha: .05),
+                      blurRadius: 1,
+                      spreadRadius: 1,
+                    ),
+                  ],
+                ),
+                child: TaskActivityWidget(),
+              ),
+            ),
+          ),
+          Positioned.fill(
             top: 320,
             left: 42,
             right: -24,
