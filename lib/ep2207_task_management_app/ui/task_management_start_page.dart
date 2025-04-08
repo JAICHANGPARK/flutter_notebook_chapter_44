abf75e359244020f23d02022eb38f73feef91b78
JAICHANGPARK <aristojeff@gmail.com>
2025-04-08 18:24:49 +0900

    Create task_management_start_page.dart

---
+++ lib/ep2207_task_management_app/ui/task_management_start_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class TaskManagementStartPage extends StatefulWidget {
+  const TaskManagementStartPage({super.key});
+
+  @override
+  State<TaskManagementStartPage> createState() => _TaskManagementStartPageState();
+}
+
+class _TaskManagementStartPageState extends State<TaskManagementStartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
