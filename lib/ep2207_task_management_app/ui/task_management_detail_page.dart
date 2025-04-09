7d53fb45b47aa6bd04d3adcac24040283f32f3c8
JAICHANGPARK <aristojeff@gmail.com>
2025-04-09 20:00:03 +0900

    Update task_management_detail_page.dart

--- lib/ep2207_task_management_app/ui/task_management_detail_page.dart
+++ lib/ep2207_task_management_app/ui/task_management_detail_page.dart
@@ -176,6 +176,7 @@ class _TaskManagementDetailPageState extends State<TaskManagementDetailPage> {
                     Text("About this Project"),
                     Text(
                       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                      style: TextStyle(color: Colors.grey),
                     ),
                   ],
                 ),
