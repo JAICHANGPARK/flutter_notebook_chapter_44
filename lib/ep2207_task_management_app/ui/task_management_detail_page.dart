d75cbc63785445d9634bb2cfd97cf105b94455ef
JAICHANGPARK <aristojeff@gmail.com>
2025-04-09 19:51:32 +0900

    Update task_management_detail_page.dart

--- lib/ep2207_task_management_app/ui/task_management_detail_page.dart
+++ lib/ep2207_task_management_app/ui/task_management_detail_page.dart
@@ -4,7 +4,8 @@ class TaskManagementDetailPage extends StatefulWidget {
   const TaskManagementDetailPage({super.key});
 
   @override
-  State<TaskManagementDetailPage> createState() => _TaskManagementDetailPageState();
+  State<TaskManagementDetailPage> createState() =>
+      _TaskManagementDetailPageState();
 }
 
 class _TaskManagementDetailPageState extends State<TaskManagementDetailPage> {
@@ -12,9 +13,21 @@ class _TaskManagementDetailPageState extends State<TaskManagementDetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(child: Column(children: [
-        
-      ],)),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+
+          children: [
+            Row(
+              children: [
+                CircleAvatar(),
+                Expanded(child: Center(child: Text("Details"))),
+                CircleAvatar(),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
