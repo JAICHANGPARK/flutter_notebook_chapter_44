5d20f12612ad0c815ed33a48f2affb92a72cf88a
JAICHANGPARK <aristojeff@gmail.com>
2025-04-09 19:50:17 +0900

    Update task_management_detail_page.dart

---
+++ lib/ep2207_task_management_app/ui/task_management_detail_page.dart
@@ -0,0 +1,13 @@
+class TaskManagementDetailPage extends StatefulWidget {
+  const TaskManagementDetailPage({super.key});
+
+  @override
+  State<TaskManagementDetailPage> createState() => _TaskManagementDetailPageState();
+}
+
+class _TaskManagementDetailPageState extends State<TaskManagementDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
