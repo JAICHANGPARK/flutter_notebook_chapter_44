f19dd66e0be6090d8375b7648e269b41634ed778
JAICHANGPARK <aristojeff@gmail.com>
2025-04-11 18:44:27 +0900

    Create ep2212_food_delivery_app.dart

---
+++ lib/ep2212_food_delivery_app/ep2212_food_delivery_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+}
+
+
+class FoodDeliveryApp extends StatelessWidget {
+  const FoodDeliveryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
