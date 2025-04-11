8d2296cc545a9692de68ada7ad00d59720374498
JAICHANGPARK <aristojeff@gmail.com>
2025-04-11 18:45:33 +0900

    Update ep2212_food_delivery_app.dart

--- lib/ep2212_food_delivery_app/ep2212_food_delivery_app.dart
+++ lib/ep2212_food_delivery_app/ep2212_food_delivery_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_44/ep2212_food_delivery_app/ui/food_delivery_home_page.dart';
 
 void main() {
 
@@ -11,7 +12,7 @@ class FoodDeliveryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ,
+      home: FoodDeliveryHomePage(),
     );
   }
 }
