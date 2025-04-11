f920b337cee2075f1b687c431f105d2bf0757dd9
JAICHANGPARK <aristojeff@gmail.com>
2025-04-11 18:45:46 +0900

    Update ep2212_food_delivery_app.dart

--- lib/ep2212_food_delivery_app/ep2212_food_delivery_app.dart
+++ lib/ep2212_food_delivery_app/ep2212_food_delivery_app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_44/ep2212_food_delivery_app/ui/food_delivery_home_page.dart';
 
 void main() {
-
+    runApp(FoodDeliveryApp());
 }
 
 
