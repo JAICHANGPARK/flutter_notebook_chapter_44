f92c55113685a03a2cb1b29b097cdc9a995dc2f8
JAICHANGPARK <aristojeff@gmail.com>
2025-04-13 08:28:51 +0900

    Update food_delivery_detail_page.dart

---
+++ lib/ep2212_food_delivery_app/ui/food_delivery_detail_page.dart
@@ -0,0 +1,14 @@
+
+class FoodDeliveryDetailPage extends StatefulWidget {
+  const FoodDeliveryDetailPage({super.key});
+
+  @override
+  State<FoodDeliveryDetailPage> createState() => _FoodDeliveryDetailPageState();
+}
+
+class _FoodDeliveryDetailPageState extends State<FoodDeliveryDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
