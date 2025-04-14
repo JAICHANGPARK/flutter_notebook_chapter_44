3c5928cbad0f976009554ceafcecc835a3888a55
JAICHANGPARK <aristojeff@gmail.com>
2025-04-14 18:25:12 +0900

    Update food_delivery_cart_page.dart

--- lib/ep2212_food_delivery_app/ui/food_delivery_cart_page.dart
+++ lib/ep2212_food_delivery_app/ui/food_delivery_cart_page.dart
@@ -11,6 +11,8 @@ class FoodDeliveryCartPage extends StatefulWidget {
 class _FoodDeliveryCartPageState extends State<FoodDeliveryCartPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+    );
   }
 }
