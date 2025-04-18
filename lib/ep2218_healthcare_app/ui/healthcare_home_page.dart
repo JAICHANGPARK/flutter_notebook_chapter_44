776a99024fb9320321f7fba6318e6cbaff38e7b8
JAICHANGPARK <aristojeff@gmail.com>
2025-04-18 17:50:39 +0900

    Update healthcare_home_page.dart

--- lib/ep2218_healthcare_app/ui/healthcare_home_page.dart
+++ lib/ep2218_healthcare_app/ui/healthcare_home_page.dart
@@ -17,6 +17,8 @@ class _HealthcareHomePageState extends State<HealthcareHomePage> {
           Positioned.fill(
             child: SafeArea(
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                spacing: 16,
                 children: [
                   Padding(
                     padding: const EdgeInsets.all(16.0),
@@ -40,16 +42,21 @@ class _HealthcareHomePageState extends State<HealthcareHomePage> {
                           radius: 24,
                           backgroundColor: Colors.white.withValues(alpha: .2),
                           foregroundColor: Colors.white,
-                          child: Badge(
-
-                            child: Icon(Icons.notifications_none),
-                          )
+                          child: Badge(child: Icon(Icons.notifications_none)),
                         ),
                         CircleAvatar(
                           radius: 28,
                           backgroundColor: Colors.white.withValues(alpha: .2),
                         ),
                       ],
+                    ),
+                  ),
+                  Text(
+                    "Book a Doctor",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ],
