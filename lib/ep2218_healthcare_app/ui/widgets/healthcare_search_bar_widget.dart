71989c0609858dc328b3dd0434b8e3969a3a2592
JAICHANGPARK <aristojeff@gmail.com>
2025-04-19 07:52:29 +0900

    Update healthcare_search_bar_widget.dart

---
+++ lib/ep2218_healthcare_app/ui/widgets/healthcare_search_bar_widget.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class HealthcareSearchBarWidget extends StatelessWidget {
+  const HealthcareSearchBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return    Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        spacing: 12,
+        children: [
+          Text(
+            "Book a Doctor",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Row(
+            spacing: 8,
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: ShapeDecoration(
+                    shape: StadiumBorder(),
+                    color: Colors.white.withValues(alpha: .2),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Search",
+                      icon: Icon(Icons.search),
+                      iconColor: Colors.white,
+                      hintStyle: TextStyle(color: Colors.white),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+              CircleAvatar(
+                radius: 24,
+                backgroundColor: Colors.white.withValues(
+                  alpha: .2,
+                ),
+                foregroundColor: Colors.white,
+                child: Icon(Icons.tune),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
