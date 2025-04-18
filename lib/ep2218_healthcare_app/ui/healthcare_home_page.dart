44a446d337a53f194672b4625f628ea3850f2ab2
JAICHANGPARK <aristojeff@gmail.com>
2025-04-19 07:53:20 +0900

    Update healthcare_home_page.dart

--- lib/ep2218_healthcare_app/ui/healthcare_home_page.dart
+++ lib/ep2218_healthcare_app/ui/healthcare_home_page.dart
@@ -22,37 +22,7 @@ class _HealthcareHomePageState extends State<HealthcareHomePage> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 spacing: 16,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Row(
-                      spacing: 8,
-                      children: [
-                        CircleAvatar(
-                          radius: 24,
-                          backgroundColor: Colors.white.withValues(alpha: .2),
-                          foregroundColor: Colors.white,
-                          child: Icon(Icons.arrow_back),
-                        ),
-                        Spacer(),
-                        CircleAvatar(
-                          radius: 24,
-                          backgroundColor: Colors.white.withValues(alpha: .2),
-                          foregroundColor: Colors.white,
-                          child: Icon(Icons.calendar_month),
-                        ),
-                        CircleAvatar(
-                          radius: 24,
-                          backgroundColor: Colors.white.withValues(alpha: .2),
-                          foregroundColor: Colors.white,
-                          child: Badge(child: Icon(Icons.notifications_none)),
-                        ),
-                        CircleAvatar(
-                          radius: 28,
-                          backgroundColor: Colors.white.withValues(alpha: .2),
-                        ),
-                      ],
-                    ),
-                  ),
+
                   HealthcareSearchBarWidget(),
                   Container(
                     height: 52,
