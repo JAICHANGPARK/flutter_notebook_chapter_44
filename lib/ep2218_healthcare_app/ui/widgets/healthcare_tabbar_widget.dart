7b6c2f4920971d7726a5a135883a0e94dba95e81
JAICHANGPARK <aristojeff@gmail.com>
2025-04-19 07:54:30 +0900

    Update healthcare_tabbar_widget.dart

--- lib/ep2218_healthcare_app/ui/widgets/healthcare_tabbar_widget.dart
+++ lib/ep2218_healthcare_app/ui/widgets/healthcare_tabbar_widget.dart
@@ -5,6 +5,49 @@ class HealthcareTabbarWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      height: 52,
+      padding: EdgeInsets.only(left: 16),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            Container(
+              margin: EdgeInsets.only(right: 8),
+              decoration: ShapeDecoration(
+                shape: StadiumBorder(),
+                color: Color.fromRGBO(203, 251, 96, 1),
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+              child: Text("Today, 26 Jul", style: TextStyle(fontSize: 16)),
+            ),
+            Container(
+              margin: EdgeInsets.only(right: 8),
+              decoration: ShapeDecoration(
+                shape: StadiumBorder(),
+                color: Colors.white.withValues(alpha: .15),
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+              child: Text(
+                "Tomorrow, 27 Jul",
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(right: 8),
+              decoration: ShapeDecoration(
+                shape: StadiumBorder(),
+                color: Colors.white.withValues(alpha: .15),
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+              child: Text(
+                "Day after, 28 Jul",
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
