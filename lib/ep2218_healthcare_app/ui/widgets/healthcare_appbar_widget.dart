938630496c91797ed5b7f3818733cd536c60ccb7
JAICHANGPARK <aristojeff@gmail.com>
2025-04-19 07:53:18 +0900

    Update healthcare_appbar_widget.dart

--- lib/ep2218_healthcare_app/ui/widgets/healthcare_appbar_widget.dart
+++ lib/ep2218_healthcare_app/ui/widgets/healthcare_appbar_widget.dart
@@ -6,6 +6,36 @@ class HealthcareAppbarWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        spacing: 8,
+        children: [
+          CircleAvatar(
+            radius: 24,
+            backgroundColor: Colors.white.withValues(alpha: .2),
+            foregroundColor: Colors.white,
+            child: Icon(Icons.arrow_back),
+          ),
+          Spacer(),
+          CircleAvatar(
+            radius: 24,
+            backgroundColor: Colors.white.withValues(alpha: .2),
+            foregroundColor: Colors.white,
+            child: Icon(Icons.calendar_month),
+          ),
+          CircleAvatar(
+            radius: 24,
+            backgroundColor: Colors.white.withValues(alpha: .2),
+            foregroundColor: Colors.white,
+            child: Badge(child: Icon(Icons.notifications_none)),
+          ),
+          CircleAvatar(
+            radius: 28,
+            backgroundColor: Colors.white.withValues(alpha: .2),
+          ),
+        ],
+      ),
+    );
   }
 }
