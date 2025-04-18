c0575e71b1f7bb40d55f8f8dfdf5deacd5e956fe
JAICHANGPARK <aristojeff@gmail.com>
2025-04-18 17:41:57 +0900

    Update ep2218_main.dart

--- lib/ep2218_healthcare_app/ep2218_main.dart
+++ lib/ep2218_healthcare_app/ep2218_main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 
-class Ep2218Main extends StatelessWidget {
-  const Ep2218Main({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+void main() {
+
 }
+
