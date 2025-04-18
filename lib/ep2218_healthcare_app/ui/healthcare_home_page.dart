3e3fcbaf78cdf65078b557893208e0aa8e3ebcf6
JAICHANGPARK <aristojeff@gmail.com>
2025-04-19 07:52:30 +0900

    Update healthcare_home_page.dart

--- lib/ep2218_healthcare_app/ui/healthcare_home_page.dart
+++ lib/ep2218_healthcare_app/ui/healthcare_home_page.dart
@@ -51,54 +51,7 @@ class _HealthcareHomePageState extends State<HealthcareHomePage> {
                       ],
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      spacing: 12,
-                      children: [
-                        Text(
-                          "Book a Doctor",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 32,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Row(
-                          spacing: 8,
-                          children: [
-                            Expanded(
-                              child: Container(
-                                decoration: ShapeDecoration(
-                                  shape: StadiumBorder(),
-                                  color: Colors.white.withValues(alpha: .2),
-                                ),
-                                padding: EdgeInsets.symmetric(horizontal: 16),
-                                child: TextField(
-                                  decoration: InputDecoration(
-                                    hintText: "Search",
-                                    icon: Icon(Icons.search),
-                                    iconColor: Colors.white,
-                                    hintStyle: TextStyle(color: Colors.white),
-                                    border: InputBorder.none,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            CircleAvatar(
-                              radius: 24,
-                              backgroundColor: Colors.white.withValues(
-                                alpha: .2,
-                              ),
-                              foregroundColor: Colors.white,
-                              child: Icon(Icons.tune),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
+
                   Container(
                     height: 52,
                     padding: EdgeInsets.only(left: 16),
