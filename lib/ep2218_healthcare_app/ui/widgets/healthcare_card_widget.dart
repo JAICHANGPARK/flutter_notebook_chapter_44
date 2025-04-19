d415caa478d93997ec5e91b6de2bfd7f59ff1fba
JAICHANGPARK <aristojeff@gmail.com>
2025-04-20 08:38:33 +0900

    Update healthcare_card_widget.dart

---
+++ lib/ep2218_healthcare_app/ui/widgets/healthcare_card_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class HealthcareCardWidget extends StatelessWidget {
+  const HealthcareCardWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
