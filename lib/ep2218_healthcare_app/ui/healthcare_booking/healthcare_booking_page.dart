d167c4154bfd8b18eaacf6bcb041b0b7665546fa
JAICHANGPARK <aristojeff@gmail.com>
2025-04-21 18:25:49 +0900

    Update healthcare_booking_page.dart

--- lib/ep2218_healthcare_app/ui/healthcare_booking/healthcare_booking_page.dart
+++ lib/ep2218_healthcare_app/ui/healthcare_booking/healthcare_booking_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_44/ep2218_healthcare_app/ui/common/healthcare_nav_back_button.dart';
 import 'package:gap/gap.dart';
-
 
 class HealthcareBookingPage extends StatefulWidget {
   const HealthcareBookingPage({super.key});
@@ -12,20 +12,23 @@ class HealthcareBookingPage extends StatefulWidget {
 class _HealthcareBookingPageState extends State<HealthcareBookingPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      spacing: 16,
-      children: [
-        Gap(24),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Row(
-            children: [
-              HealthcareNavBackButton(),
-            ],
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        spacing: 16,
+        children: [
+          Gap(24),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                HealthcareNavBackButton(),
+                Expanded(child: Center(child: Text("Doctor Booking"))),
+              ],
+            ),
           ),
-        )
-      ],
+        ],
+      ),
     );
   }
 }
