7d9b5bca5be133116e4905ed8eadf6a7d22b80cb
JAICHANGPARK <aristojeff@gmail.com>
2025-04-21 18:27:02 +0900

    Update healthcare_alert_button.dart

--- lib/ep2218_healthcare_app/ui/common/healthcare_alert_button.dart
+++ lib/ep2218_healthcare_app/ui/common/healthcare_alert_button.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 
-
 class HealthcareAlertButton extends StatelessWidget {
   const HealthcareAlertButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  CircleAvatar(
+    return CircleAvatar(
       radius: 24,
       backgroundColor: Colors.white.withValues(alpha: .2),
       foregroundColor: Colors.white,
