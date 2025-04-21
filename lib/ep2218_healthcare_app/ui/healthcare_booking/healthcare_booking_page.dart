7ee6bac478629973943023484f6c4c77e706ccfe
JAICHANGPARK <aristojeff@gmail.com>
2025-04-21 18:22:03 +0900

    Update healthcare_booking_page.dart

--- lib/ep2218_healthcare_app/ui/healthcare_booking/healthcare_booking_page.dart
+++ lib/ep2218_healthcare_app/ui/healthcare_booking/healthcare_booking_page.dart
@@ -1,1 +1,20 @@
 import 'package:flutter/material.dart';
+
+
+class HealthcareBookingPage extends StatefulWidget {
+  const HealthcareBookingPage({super.key});
+
+  @override
+  State<HealthcareBookingPage> createState() => _HealthcareBookingPageState();
+}
+
+class _HealthcareBookingPageState extends State<HealthcareBookingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}
