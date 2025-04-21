5fd5cf6a0f1b287229ff3ad5fd836ff150dcdb9a
JAICHANGPARK <aristojeff@gmail.com>
2025-04-21 18:25:07 +0900

    Update healthcare_nav_back_button.dart

--- lib/ep2218_healthcare_app/ui/common/healthcare_nav_back_button.dart
+++ lib/ep2218_healthcare_app/ui/common/healthcare_nav_back_button.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 
-
 class HealthcareNavBackButton extends StatelessWidget {
   const HealthcareNavBackButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  CircleAvatar(
+    return CircleAvatar(
       radius: 24,
       backgroundColor: Colors.white.withValues(alpha: .2),
       foregroundColor: Colors.white,
