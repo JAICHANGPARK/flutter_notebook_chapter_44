ac789af58120308a473846ddd313f63838257ca9
JAICHANGPARK <aristojeff@gmail.com>
2025-04-21 18:29:28 +0900

    Update healthcare_booking_page.dart

--- lib/ep2218_healthcare_app/ui/healthcare_booking/healthcare_booking_page.dart
+++ lib/ep2218_healthcare_app/ui/healthcare_booking/healthcare_booking_page.dart
@@ -38,6 +38,7 @@ class _HealthcareBookingPageState extends State<HealthcareBookingPage> {
                 ],
               ),
             ),
+            HealthcareBookingTabbarWidget(),
           ],
         ),
       ),
