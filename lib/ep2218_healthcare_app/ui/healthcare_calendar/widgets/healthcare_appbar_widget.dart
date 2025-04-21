a14b65addba6bc385a64ba34f03e19c25fe018a3
JAICHANGPARK <aristojeff@gmail.com>
2025-04-21 18:27:25 +0900

    Update healthcare_appbar_widget.dart

--- lib/ep2218_healthcare_app/ui/healthcare_calendar/widgets/healthcare_appbar_widget.dart
+++ lib/ep2218_healthcare_app/ui/healthcare_calendar/widgets/healthcare_appbar_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_44/ep2218_healthcare_app/ui/common/healthcare_alert_button.dart';
 import 'package:flutter_notebook_chapter_44/ep2218_healthcare_app/ui/common/healthcare_nav_back_button.dart';
 
 class HealthcareAppbarWidget extends StatelessWidget {
@@ -19,12 +20,7 @@ class HealthcareAppbarWidget extends StatelessWidget {
             foregroundColor: Colors.white,
             child: Icon(Icons.calendar_month),
           ),
-          CircleAvatar(
-            radius: 24,
-            backgroundColor: Colors.white.withValues(alpha: .2),
-            foregroundColor: Colors.white,
-            child: Badge(child: Icon(Icons.notifications_none)),
-          ),
+          HealthcareAlertButton(),
           CircleAvatar(
             radius: 28,
             backgroundColor: Colors.white.withValues(alpha: .2),
