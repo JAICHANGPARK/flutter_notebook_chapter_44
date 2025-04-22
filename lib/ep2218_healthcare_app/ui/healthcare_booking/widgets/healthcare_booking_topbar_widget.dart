cfe5be5865d43497deb52033353db4c1afc0510b
JAICHANGPARK <aristojeff@gmail.com>
2025-04-22 18:35:28 +0900

    Update healthcare_booking_topbar_widget.dart

---
+++ lib/ep2218_healthcare_app/ui/healthcare_booking/widgets/healthcare_booking_topbar_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class HealthcareBookingTopbarWidget extends StatelessWidget {
+  const HealthcareBookingTopbarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
