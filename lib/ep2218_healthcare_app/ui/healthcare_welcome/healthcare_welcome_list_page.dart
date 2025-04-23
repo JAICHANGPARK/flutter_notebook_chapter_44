8fae982017017e8edca2556478a3eb9b38e521f1
JAICHANGPARK <aristojeff@gmail.com>
2025-04-23 19:18:09 +0900

    Update healthcare_welcome_list_page.dart

--- lib/ep2218_healthcare_app/ui/healthcare_welcome/healthcare_welcome_list_page.dart
+++ lib/ep2218_healthcare_app/ui/healthcare_welcome/healthcare_welcome_list_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_44/ep2218_healthcare_app/ui/common/healthcare_alert_button.dart';
+import 'package:flutter_notebook_chapter_44/ep2218_healthcare_app/ui/common/healthcare_nav_back_button.dart';
 
 class HealthcareWelcomeListPage extends StatefulWidget {
   const HealthcareWelcomeListPage({super.key});
@@ -11,27 +13,49 @@ class HealthcareWelcomeListPage extends StatefulWidget {
 class _HealthcareWelcomeListPageState extends State<HealthcareWelcomeListPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(),
-            color: Color.fromRGBO(203, 251, 96, 1),
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(),
+              color: Color.fromRGBO(203, 251, 96, 1),
+            ),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    HealthcareNavBackButton(),
+                    Expanded(child: Center(child: Text("Welcome List"))),
+                    HealthcareAlertButton(),
+                  ],
+                ),
+              ],
+            ),
           ),
-          child: Column(children: []),
-        ),
-        Expanded(child: Column(children: [
-          Row(
-            children: [
-              Text("Doctor List"),
-              CircleAvatar(
-                child: Icon(Icons.arrow_forward),
-              ),
-            ],
-          )
-
-        ])),
-      ],
+          Expanded(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Text("Doctor List"),
+                    CircleAvatar(child: Icon(Icons.arrow_forward)),
+                  ],
+                ),
+                Expanded(
+                  child: ListView.builder(itemBuilder: (context, index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        
+                      ),
+                    );
+                  }),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
