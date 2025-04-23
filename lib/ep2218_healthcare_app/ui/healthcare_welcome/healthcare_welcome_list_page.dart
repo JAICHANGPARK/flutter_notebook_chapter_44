68a38e78b60a1a18d2030eae7799c013d5811530
JAICHANGPARK <aristojeff@gmail.com>
2025-04-23 19:09:54 +0900

    Update healthcare_welcome_list_page.dart

--- lib/ep2218_healthcare_app/ui/healthcare_welcome/healthcare_welcome_list_page.dart
+++ lib/ep2218_healthcare_app/ui/healthcare_welcome/healthcare_welcome_list_page.dart
@@ -1,3 +1,16 @@
 import 'package:flutter/material.dart';
 
 
+class HealthcareWelcomeListPage extends StatefulWidget {
+  const HealthcareWelcomeListPage({super.key});
+
+  @override
+  State<HealthcareWelcomeListPage> createState() => _HealthcareWelcomeListPageState();
+}
+
+class _HealthcareWelcomeListPageState extends State<HealthcareWelcomeListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
