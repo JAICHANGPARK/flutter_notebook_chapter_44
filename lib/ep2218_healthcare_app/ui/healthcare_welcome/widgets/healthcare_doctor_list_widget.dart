a32425b54506f98bf2479868e279cb37516b0494
JAICHANGPARK <aristojeff@gmail.com>
2025-04-25 19:36:04 +0900

    Update healthcare_doctor_list_widget.dart

---
+++ lib/ep2218_healthcare_app/ui/healthcare_welcome/widgets/healthcare_doctor_list_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class HealthcareDoctorListWidget extends StatelessWidget {
+  const HealthcareDoctorListWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
