e7f795112b47086de1edad7259f81f93600e1a72
JAICHANGPARK <aristojeff@gmail.com>
2025-04-26 08:33:15 +0900

    Update health_main_page.dart

--- lib/ep2228_health_mobile_app/ui/health_main_page.dart
+++ lib/ep2228_health_mobile_app/ui/health_main_page.dart
@@ -11,6 +11,10 @@ class _HealthMainPageState extends State<HealthMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.arrow_back_ios_new),
+        
+      ),
       bottomNavigationBar: SizedBox(
         height: 80,
         child: BottomNavigationBar(
