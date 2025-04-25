c1c5c1efd4f273e0115f4bfd6163f2fd7e1a5311
JAICHANGPARK <aristojeff@gmail.com>
2025-04-26 08:30:48 +0900

    Update ep2228_main.dart

--- lib/ep2228_health_mobile_app/ep2228_main.dart
+++ lib/ep2228_health_mobile_app/ep2228_main.dart
@@ -2,11 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'ui/health_main_page.dart';
 
-
 void main() {
   runApp(HealthMobileApp());
 }
-
 
 class HealthMobileApp extends StatelessWidget {
   const HealthMobileApp({super.key});
@@ -14,6 +12,7 @@ class HealthMobileApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(scaffoldBackgroundColor: Colors.black),
       home: HealthMainPage(),
     );
   }
