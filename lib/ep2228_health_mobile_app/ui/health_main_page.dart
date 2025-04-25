0140248d34c36717342bb084aaa41903de5befd6
JAICHANGPARK <aristojeff@gmail.com>
2025-04-26 08:27:39 +0900

    Update health_main_page.dart

--- lib/ep2228_health_mobile_app/ui/health_main_page.dart
+++ lib/ep2228_health_mobile_app/ui/health_main_page.dart
@@ -1,3 +1,16 @@
 import 'package:flutter/material.dart';
 
 
+class HealthMainPage extends StatefulWidget {
+  const HealthMainPage({super.key});
+
+  @override
+  State<HealthMainPage> createState() => _HealthMainPageState();
+}
+
+class _HealthMainPageState extends State<HealthMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
