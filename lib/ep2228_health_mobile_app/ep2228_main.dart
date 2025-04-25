d07da9a31cff610ca9ac7c2ce604b31ebb78826a
JAICHANGPARK <aristojeff@gmail.com>
2025-04-26 08:27:41 +0900

    Update ep2228_main.dart

--- lib/ep2228_health_mobile_app/ep2228_main.dart
+++ lib/ep2228_health_mobile_app/ep2228_main.dart
@@ -11,6 +11,8 @@ class HealthMobileApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: HealthMainPage(),
+    );
   }
 }
