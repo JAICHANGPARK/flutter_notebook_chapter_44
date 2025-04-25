44669cfc04f3b63a0830f72e14303df252ca8454
JAICHANGPARK <aristojeff@gmail.com>
2025-04-26 08:29:55 +0900

    Update health_main_page.dart

--- lib/ep2228_health_mobile_app/ui/health_main_page.dart
+++ lib/ep2228_health_mobile_app/ui/health_main_page.dart
@@ -16,7 +16,7 @@ class _HealthMainPageState extends State<HealthMainPage> {
           BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: "Home"),
           BottomNavigationBarItem(icon: Icon(Icons.stacked_bar_chart), label: "Statistics"),
           BottomNavigationBarItem(icon: Icon(Icons.search_outlined), label: "Catalog"),
-          BottomNavigationBarItem(icon: Icon(Icons.settings_outlined), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.settings_outlined), label: "Settings"),
         ],
       ),
     );
