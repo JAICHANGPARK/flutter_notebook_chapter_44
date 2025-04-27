570315a4a2b9ea8f519dd12da885013486f7f20b
JAICHANGPARK <aristojeff@gmail.com>
2025-04-27 09:00:35 +0900

    Update health_home_page.dart

--- lib/ep2228_health_mobile_app/ui/home/health_home_page.dart
+++ lib/ep2228_health_mobile_app/ui/home/health_home_page.dart
@@ -10,49 +10,53 @@ class HealthHomePage extends StatefulWidget {
 class _HealthHomePageState extends State<HealthHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        AppBar(
-          backgroundColor: Colors.black,
-          foregroundColor: Colors.white,
-          leading: Icon(Icons.arrow_back_ios_new),
-          title: Text("Today, Mar 12"),
-          centerTitle: true,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [Text("Breakfast"), CircleAvatar(child: Icon(Icons.add))],
-        ),
-        Row(
-          children: [
-            Text("626"),
-            Text("kcal"),
-            Container(
-              decoration: ShapeDecoration(
-                shape: StadiumBorder(),
-                color: Colors.green,
-              ),
-              child: Text("Normal", style: TextStyle(color: Colors.white)),
-            ),
-          ],
-        ),
-        Expanded(
-          child: ListView(
+    return SafeArea(
+      child: Column(
+        children: [
+          AppBar(
+            backgroundColor: Colors.black,
+            foregroundColor: Colors.white,
+            leading: Icon(Icons.arrow_back_ios_new),
+            title: Text("Today, Mar 12"),
+            centerTitle: true,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [Text("Breakfast"), CircleAvatar(child: Icon(Icons.add))],
+          ),
+          Row(
             children: [
+              Text("626"),
+              Text("kcal"),
               Container(
-                decoration: BoxDecoration(color: Colors.pink[50]!),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('fried eggs with tomatoes and bacon'),
-                    Row(children: [Text("410"), Text("kcal"), CircleAvatar()]),
-                  ],
+                decoration: ShapeDecoration(
+                  shape: StadiumBorder(),
+                  color: Colors.green,
                 ),
+                child: Text("Normal", style: TextStyle(color: Colors.white)),
               ),
             ],
           ),
-        ),
-      ],
+          Expanded(
+            child: ListView(
+              children: [
+                Container(
+                  decoration: BoxDecoration(color: Colors.pink[50]!),
+                  padding: EdgeInsets.all(24),
+                  child: Column(
+                    
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('fried eggs with tomatoes and bacon'),
+                      Row(children: [Text("410"), Text("kcal"), CircleAvatar()]),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
