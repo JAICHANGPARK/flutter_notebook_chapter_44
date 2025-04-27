62171ffa7ea609f784d74590741ad947febf5bf4
JAICHANGPARK <aristojeff@gmail.com>
2025-04-27 09:03:15 +0900

    Update health_home_page.dart

--- lib/ep2228_health_mobile_app/ui/home/health_home_page.dart
+++ lib/ep2228_health_mobile_app/ui/home/health_home_page.dart
@@ -64,7 +64,7 @@ class _HealthHomePageState extends State<HealthHomePage> {
                       ],
                     ),
                   ),
-                  Container(height: 140,
+                  Container(height: 160,
                     decoration: BoxDecoration(color: Colors.pink[50]!),
                     padding: EdgeInsets.all(24),
                     margin: EdgeInsets.only(bottom: 8),
