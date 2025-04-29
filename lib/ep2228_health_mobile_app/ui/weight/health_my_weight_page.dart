a134dbbf0d37fcf9bd7a30e500334a3ca9dd5002
JAICHANGPARK <aristojeff@gmail.com>
2025-04-29 18:25:58 +0900

    Update health_my_weight_page.dart

---
+++ lib/ep2228_health_mobile_app/ui/weight/health_my_weight_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class HealthMyWeightPage extends StatefulWidget {
+  const HealthMyWeightPage({super.key});
+
+  @override
+  State<HealthMyWeightPage> createState() => _HealthMyWeightPageState();
+}
+
+class _HealthMyWeightPageState extends State<HealthMyWeightPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
