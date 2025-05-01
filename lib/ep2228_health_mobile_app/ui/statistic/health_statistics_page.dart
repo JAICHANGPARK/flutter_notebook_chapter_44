f79d92637fecc22b78aa8bcb49cb3d154cc48b50
JAICHANGPARK <aristojeff@gmail.com>
2025-05-01 10:35:53 +0900

    Update health_statistics_page.dart

---
+++ lib/ep2228_health_mobile_app/ui/statistic/health_statistics_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class HealthStatisticsPage extends StatefulWidget {
+  const HealthStatisticsPage({super.key});
+
+  @override
+  State<HealthStatisticsPage> createState() => _HealthStatisticsPageState();
+}
+
+class _HealthStatisticsPageState extends State<HealthStatisticsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
