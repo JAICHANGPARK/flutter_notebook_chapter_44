5ed698e224a5551c664d33ca70872fb41c4cacdd
JAICHANGPARK <aristojeff@gmail.com>
2025-05-01 10:38:52 +0900

    Update health_statistics_page.dart

--- lib/ep2228_health_mobile_app/ui/statistic/health_statistics_page.dart
+++ lib/ep2228_health_mobile_app/ui/statistic/health_statistics_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_notebook_chapter_44/ep2228_health_mobile_app/ui/weight/widget/chart_legend_item_widget.dart';
 
 class HealthStatisticsPage extends StatefulWidget {
   const HealthStatisticsPage({super.key});
@@ -11,6 +11,39 @@ class HealthStatisticsPage extends StatefulWidget {
 class _HealthStatisticsPageState extends State<HealthStatisticsPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text("Current week", style: TextStyle(color: Colors.white,
+            fontSize: 24,
+            )),
+            IconButton(onPressed: (){}, icon: Icon(Icons.calendar_month)),
+          ],
+        ),
+        Divider(),
+        Row(
+          spacing: 24,
+          children: [
+            ChartLegendItemWidget(
+              color: Colors.pink.shade300,
+              text: "fat",
+            ),
+            ChartLegendItemWidget(
+              color: Colors.orange.shade300,
+              text: "protein",
+            ),
+            ChartLegendItemWidget(
+              color: Colors.green,
+              text: "carbs",
+            ),
+            ChartLegendItemWidget(
+              color: Colors.white,
+              text: "activity",
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
