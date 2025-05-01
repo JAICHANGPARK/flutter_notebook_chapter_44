4da6c468fa04e2d588110487192c3dd65f5ca9bf
JAICHANGPARK <aristojeff@gmail.com>
2025-05-01 10:39:50 +0900

    Update health_statistics_page.dart

--- lib/ep2228_health_mobile_app/ui/statistic/health_statistics_page.dart
+++ lib/ep2228_health_mobile_app/ui/statistic/health_statistics_page.dart
@@ -11,39 +11,52 @@ class HealthStatisticsPage extends StatefulWidget {
 class _HealthStatisticsPageState extends State<HealthStatisticsPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            Text("Current week", style: TextStyle(color: Colors.white,
-            fontSize: 24,
-            )),
-            IconButton(onPressed: (){}, icon: Icon(Icons.calendar_month)),
-          ],
-        ),
-        Divider(),
-        Row(
-          spacing: 24,
-          children: [
-            ChartLegendItemWidget(
-              color: Colors.pink.shade300,
-              text: "fat",
-            ),
-            ChartLegendItemWidget(
-              color: Colors.orange.shade300,
-              text: "protein",
-            ),
-            ChartLegendItemWidget(
-              color: Colors.green,
-              text: "carbs",
-            ),
-            ChartLegendItemWidget(
-              color: Colors.white,
-              text: "activity",
-            ),
-          ],
-        ),
-      ],
+    return SafeArea(
+      child: Column(
+        children: [
+
+          Row(
+            children: [
+              Text("Current week", style: TextStyle(color: Colors.white,
+              fontSize: 24,
+              )),
+              IconButton(onPressed: (){}, icon: Icon(Icons.calendar_month)),
+            ],
+          ),
+          Divider(),
+          Row(
+            spacing: 24,
+            children: [
+              ChartLegendItemWidget(
+                color: Colors.pink.shade300,
+                text: "fat",
+              ),
+              ChartLegendItemWidget(
+                color: Colors.orange.shade300,
+                text: "protein",
+              ),
+              ChartLegendItemWidget(
+                color: Colors.green,
+                text: "carbs",
+              ),
+              ChartLegendItemWidget(
+                color: Colors.white,
+                text: "activity",
+              ),
+            ],
+          ),
+          Expanded(child: Placeholder()),
+          Container(
+            height: 160,
+            child: ListView.builder(itemBuilder: (context,index){
+              return Container(
+                width: 320,
+                child: Placeholder(),
+              );
+            },),
+          )
+        ],
+      ),
     );
   }
 }
