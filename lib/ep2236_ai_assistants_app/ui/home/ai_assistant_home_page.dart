572d90b027272874ab8f88e8691720cebf7521b8
JAICHANGPARK <aristojeff@gmail.com>
2025-05-05 08:53:46 +0900

    Create ai_assistant_home_page.dart

---
+++ lib/ep2236_ai_assistants_app/ui/home/ai_assistant_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AiAssistantHomePage extends StatefulWidget {
+  const AiAssistantHomePage({super.key});
+
+  @override
+  State<AiAssistantHomePage> createState() => _AiAssistantHomePageState();
+}
+
+class _AiAssistantHomePageState extends State<AiAssistantHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
