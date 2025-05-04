e954af7063f73d60cf648d9effa1fad15820673a
JAICHANGPARK <aristojeff@gmail.com>
2025-05-04 09:30:28 +0900

    Create ai_assistant_start_page.dart

---
+++ lib/ep2236_ai_assistants_app/ui/boarding/ai_assistant_start_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class AiAssistantStartPage extends StatefulWidget {
+  const AiAssistantStartPage({super.key});
+
+  @override
+  State<AiAssistantStartPage> createState() => _AiAssistantStartPageState();
+}
+
+class _AiAssistantStartPageState extends State<AiAssistantStartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
