40ffa93696547a7769d51a97ab8e24d7bcb9f2ef
JAICHANGPARK <aristojeff@gmail.com>
2025-05-04 09:27:31 +0900

    Update ep2236_main.dart

---
+++ lib/ep2236_ai_assistants_app/ep2236_main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+
+class AIAssistantsApp extends StatelessWidget {
+  const AIAssistantsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
