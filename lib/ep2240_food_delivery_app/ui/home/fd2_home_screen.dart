2a8f247cdd09747b439a96c56470d001bfde615c
JAICHANGPARK <aristojeff@gmail.com>
2025-05-08 18:37:58 +0900

    Update fd2_home_screen.dart

---
+++ lib/ep2240_food_delivery_app/ui/home/fd2_home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class Fd2HomeScreen extends StatefulWidget {
+  const Fd2HomeScreen({super.key});
+
+  @override
+  State<Fd2HomeScreen> createState() => _Fd2HomeScreenState();
+}
+
+class _Fd2HomeScreenState extends State<Fd2HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
