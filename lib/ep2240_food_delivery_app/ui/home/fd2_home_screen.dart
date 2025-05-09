2697295947c60deecf405bbd73b13a05c3bef7e7
JAICHANGPARK <aristojeff@gmail.com>
2025-05-10 07:47:44 +0900

    Update fd2_home_screen.dart

--- lib/ep2240_food_delivery_app/ui/home/fd2_home_screen.dart
+++ lib/ep2240_food_delivery_app/ui/home/fd2_home_screen.dart
@@ -165,25 +165,27 @@ class _Fd2HomeScreenState extends State<Fd2HomeScreen> {
                     ],
                   ),
                 ),
-                Container(
-                  decoration: ShapeDecoration(
-                    color:
-                    pageIndex == 2
-                        ? Color.fromRGBO(42, 109, 62, 1)
-                        : Colors.transparent,
-                    shape: StadiumBorder(),
-                  ),
-                  padding: EdgeInsets.symmetric(horizontal: 18, vertical: 8),
-                  child: Row(
-                    spacing: 8,
-                    children: [
-                      Icon(HugeIcons.strokeRoundedShoppingBag01, color: Colors.grey),
-                      if(pageIndex == 2)
-                      Text(
-                        "Home",
-                        style: TextStyle(color: Colors.white, fontSize: 15),
-                      ),
-                    ],
+                InkWell(
+                  child: Container(
+                    decoration: ShapeDecoration(
+                      color:
+                      pageIndex == 2
+                          ? Color.fromRGBO(42, 109, 62, 1)
+                          : Colors.transparent,
+                      shape: StadiumBorder(),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 18, vertical: 8),
+                    child: Row(
+                      spacing: 8,
+                      children: [
+                        Icon(HugeIcons.strokeRoundedShoppingBag01, color: Colors.grey),
+                        if(pageIndex == 2)
+                        Text(
+                          "Home",
+                          style: TextStyle(color: Colors.white, fontSize: 15),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
