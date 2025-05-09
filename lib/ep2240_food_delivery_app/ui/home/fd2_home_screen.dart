0429c8dae86fdb8ba6a180890af9098895b9fdd7
JAICHANGPARK <aristojeff@gmail.com>
2025-05-10 07:43:45 +0900

    Update fd2_home_screen.dart

--- lib/ep2240_food_delivery_app/ui/home/fd2_home_screen.dart
+++ lib/ep2240_food_delivery_app/ui/home/fd2_home_screen.dart
@@ -119,14 +119,16 @@ class _Fd2HomeScreenState extends State<Fd2HomeScreen> {
                   decoration: ShapeDecoration(
                     color: Color.fromRGBO(42, 109, 62, 1),
                     shape: StadiumBorder(),
-
                   ),
                   padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
                   child: Row(
                     spacing: 8,
                     children: [
                       Icon(HugeIcons.strokeRoundedHome01, color: Colors.white),
-                      Text("Home", style: TextStyle(color: Colors.white)),
+                      Text(
+                        "Home",
+                        style: TextStyle(color: Colors.white, fontSize: 15),
+                      ),
                     ],
                   ),
                 ),
