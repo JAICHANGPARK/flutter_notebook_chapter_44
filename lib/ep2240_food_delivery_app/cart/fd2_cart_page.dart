a65b61b8f55bd0acb7b439e7d8eeb313bb51bfae
JAICHANGPARK <aristojeff@gmail.com>
2025-05-12 18:15:26 +0900

    Update fd2_cart_page.dart

--- lib/ep2240_food_delivery_app/cart/fd2_cart_page.dart
+++ lib/ep2240_food_delivery_app/cart/fd2_cart_page.dart
@@ -1,1 +1,16 @@
 import 'package:flutter/material.dart';
+
+
+class Fd2CartPage extends StatefulWidget {
+  const Fd2CartPage({super.key});
+
+  @override
+  State<Fd2CartPage> createState() => _Fd2CartPageState();
+}
+
+class _Fd2CartPageState extends State<Fd2CartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
