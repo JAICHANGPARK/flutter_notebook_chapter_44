e226f72c66bd52ea22918c35014ff59aeb23965a
JAICHANGPARK <aristojeff@gmail.com>
2025-05-12 18:19:07 +0900

    Update fd2_cart_page.dart

--- lib/ep2240_food_delivery_app/cart/fd2_cart_page.dart
+++ lib/ep2240_food_delivery_app/cart/fd2_cart_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class Fd2CartPage extends StatefulWidget {
   const Fd2CartPage({super.key});
 
@@ -12,30 +11,46 @@ class _Fd2CartPageState extends State<Fd2CartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Column(
-        children: [
-          Expanded(child: Placeholder()),
-          Container(
-            height: 80,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withValues(alpha: .05),
-                  spreadRadius: 10,
-                  blurRadius: 5,
-                )
-              ]
-            ),
-            child: Row(
-              children: [
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(child: Placeholder()),
+            Container(
+              height: 80,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withValues(alpha: .05),
+                    spreadRadius: 10,
+                    blurRadius: 5,
+                  ),
+                ],
+              ),
+              child: Row(
+                children: [
                   Text("US \$2.99"),
-                Container(decoration: ShapeDecoration(shape: shape),)
-              ],
+                  Container(
+                    decoration: ShapeDecoration(
+                      shape: StadiumBorder(),
+                      color: Colors.teal,
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(Icons.shopping_basket_sharp,color: Colors.white,),
+                        Text(
+                          "Add to Chart",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          )
-        ],
-      )),
+          ],
+        ),
+      ),
     );
   }
 }
