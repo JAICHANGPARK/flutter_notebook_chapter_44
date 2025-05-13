e01a6ce7891b1955b47bfed92936d883b660ccca
JAICHANGPARK <aristojeff@gmail.com>
2025-05-13 18:24:58 +0900

    Update fd2_cart_page.dart

--- lib/ep2240_food_delivery_app/cart/fd2_cart_page.dart
+++ lib/ep2240_food_delivery_app/cart/fd2_cart_page.dart
@@ -121,7 +121,10 @@ class _Fd2CartPageState extends State<Fd2CartPage> {
                                   ),
                                 ],
                               ),
-                              Text("Description"),
+                              Text("Description",style: TextStyle(
+                                fontSize: 16,
+                                
+                              ),),
                               Text(
                                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                                 maxLines: 4,
