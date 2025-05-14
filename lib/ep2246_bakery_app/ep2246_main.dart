40ccedd9bfe0b0bf41e64ec9ba748800cef8656f
JAICHANGPARK <aristojeff@gmail.com>
2025-05-14 18:27:58 +0900

    Update ep2246_main.dart

--- lib/ep2246_bakery_app/ep2246_main.dart
+++ lib/ep2246_bakery_app/ep2246_main.dart
@@ -4,3 +4,12 @@ import 'package:flutter/material.dart';
 void main() {
   
 }
+
+class BakeryApp extends StatelessWidget {
+  const BakeryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
