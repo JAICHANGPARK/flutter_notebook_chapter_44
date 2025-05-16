1079b0b05dac07ddd0aaf7920f4c2784d9ef8d32
JAICHANGPARK <aristojeff@gmail.com>
2025-05-17 07:28:01 +0900

    Update bakery_categories_page.dart

--- lib/ep2246_bakery_app/ui/categories/bakery_categories_page.dart
+++ lib/ep2246_bakery_app/ui/categories/bakery_categories_page.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 
+class BakeryCategoriesPage extends StatefulWidget {
+  const BakeryCategoriesPage({super.key});
+
+  @override
+  State<BakeryCategoriesPage> createState() => _BakeryCategoriesPageState();
+}
+
+class _BakeryCategoriesPageState extends State<BakeryCategoriesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
