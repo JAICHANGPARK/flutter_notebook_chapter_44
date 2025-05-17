44a57163f161987103c378fdae86adc4a2df8efd
JAICHANGPARK <aristojeff@gmail.com>
2025-05-18 07:46:59 +0900

    Update ep2250_main.dart

--- lib/ep2250_shopping_app/ep2250_main.dart
+++ lib/ep2250_shopping_app/ep2250_main.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  
+}
+
+
+class ShoppingApp extends StatelessWidget {
+  const ShoppingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
