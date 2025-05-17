6f39f839f4fc23a62132a9fef766ee819489b9f1
JAICHANGPARK <aristojeff@gmail.com>
2025-05-18 07:49:00 +0900

    Update shopping_main_page.dart

--- lib/ep2250_shopping_app/shop/shopping_main_page.dart
+++ lib/ep2250_shopping_app/shop/shopping_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class ShoppingMainPage extends StatefulWidget {
   const ShoppingMainPage({super.key});
 
@@ -11,6 +10,16 @@ class ShoppingMainPage extends StatefulWidget {
 class _ShoppingMainPageState extends State<ShoppingMainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            decoration: BoxDecoration(color: Color.fromRGBO(244, 246, 248, 1)),
+            
+          ),
+        ],
+      ),
+    );
   }
 }
