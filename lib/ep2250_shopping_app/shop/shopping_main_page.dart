54f7982066c9ec93c4ce8db679f0904b2bdbb3b3
JAICHANGPARK <aristojeff@gmail.com>
2025-05-18 07:47:31 +0900

    Create shopping_main_page.dart

---
+++ lib/ep2250_shopping_app/shop/shopping_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ShoppingMainPage extends StatefulWidget {
+  const ShoppingMainPage({super.key});
+
+  @override
+  State<ShoppingMainPage> createState() => _ShoppingMainPageState();
+}
+
+class _ShoppingMainPageState extends State<ShoppingMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
